f0d5904d52c65157a6d8e34f38e3994bcae6ea6a
AndyKIron <andy.kono@gmail.com>
2023-08-31 13:23:21 +0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 
 import '../node_modules/@ironsource/shared-ui/styles/levelplay/fonts.css';
-// import '@ironsource/shared-ui/styles/levelplay/fonts.css';
 
-import './style.css'
-import App from './App.vue'
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
