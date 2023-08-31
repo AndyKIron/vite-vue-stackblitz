0088bdeeac32a13f5896c959e72d68fd60e9a671
AndyKIron <andy.kononenko@unity3d.com>
2023-08-31 13:22:01 +0300

    chore: added shared-ui

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <script setup lang="ts">
-import Typography from '@ironsource/shared-ui/typography';
-import HelloWorld from './components/HelloWorld.vue'
+// import Typography from '@ironsource/shared-ui/typography';
+/*import HelloWorld from './components/HelloWorld.vue'*/
+import Wrapper from './components/Wrapper.vue'
+
+// import Banner from '@ironsource/shared-ui/banner';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper-placeholder">
+<!--    <Banner>This is a banner!</Banner>-->
+
+    <Wrapper></Wrapper>
+
+<!--
+    <br>
+    <Typography variant="chart1">SharedUI Typography component variant - chart1</Typography>
+    <br><br>
+    <div class="font-title">CSS class font-title</div>
+    <br>
+    <HelloWorld msg="Vite + Vue" />
+-->
   </div>
-  <br>
-  <Typography variant="chart1">SharedUI Typography component variant - chart1</Typography>
-  <br><br>
-  <div class="font-title">CSS class font-title</div>
-  <br>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="scss">
+@import '../node_modules/@ironsource/shared-ui/styleguide.scss';
+
+.wrapper-placeholder{
+  text-align: initial;
+  padding: 2rem;
+  margin: auto;
+  border: $border;
+  width: 978px;
 }
 </style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+// @ts-ignore TODO: fix this
+import Banner from '@ironsource/shared-ui/banner';
+</script>
+
+<template>
+  <Banner>This is a banner!</Banner>
+</template>
+
+<style scoped>
+
+</style>
